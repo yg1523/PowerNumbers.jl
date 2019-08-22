d5cbcffd286934bed5bf402ff3475e1ada5574c7
idlemat <52246739+idlemat@users.noreply.github.com>
2019-08-22 17:50:54 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,7 +34,17 @@ import PowerNumbers: PowerNumber, LogNumber, realpart, epsilon, alpha
 end
 
 @testset "Multiplication" begin
-    list = [PowerNumber(0,1,0.5),PowerNumber(0,1,-0.5),PowerNumber(3*im,1,0.2)]
+    list = [PowerNumber(0,1,0.5),PowerNumber(0,1,-0.5),PowerNumber(3,1,0.2),PowerNumber(3*im,1,0.2),PowerNumber(-3*im,1,0.2),PowerNumber(3*im,0,0.1),PowerNumber(-3*im,0,0.1),1/PowerNumber(3*im,1,0.2)]
+    for i in 1:length(list)
+        for j in i+1:length(list)
+            @test list[i]*list[j] == list[j]*list[i]
+            for k in j+1:length(list)
+                @test (list[i]*list[j])*list[k] ≈ list[i]*(list[j]*list[k])
+                @test list[i]*(list[j]+list[k]) ≈ (list[i]*list[j]) + (list[i]*list[k])
+                list[i]*(list[j]+list[k]) ≈ (list[i]*list[j]) + (list[i]*list[k]) ? print() : println(list[i],"\n",list[j],"\n",list[k],"\n")
+            end
+        end
+    end
 end
 
 @testset "PowerNumbers arithmetic" begin
