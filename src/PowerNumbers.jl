df6f9e6febab6ec3fd3ab6874fc5b57fbfac3f46
idlemat <52246739+idlemat@users.noreply.github.com>
2019-08-29 19:02:48 +0100

    Two Power Update

--- src/PowerNumbers.jl
+++ src/PowerNumbers.jl
@@ -2,9 +2,12 @@ module PowerNumbers
 
 using Base, DualNumbers, LinearAlgebra
 
-import Base: convert, *, +, -, ==, <, <=, >, |, !, !=, >=, /, ^, \, isinf, in, isapprox
-import Base: exp, atanh, log1p, abs, max, min, log, inv, real, imag, conj, sqrt, sin, cos
-
+import Base: convert, *, +, -, ==, <, <=, >, !, !=, >=, /, ^, \, in, isapprox
+import Base: exp, atanh, log1p, abs, log, inv, real, imag, conj, sqrt, 
+                sin, cos, cbrt, abs2, log10, log2, exp, exp2, expm1, 
+                sin, cos, tan, sec, csc, cot, sind, cosd, tand, secd, cscd, cotd, asin, acos, 
+                atan, asec, acsc, acot, asind, acosd, atand, asecd, acscd, acotd, sinh, cosh, 
+                tanh, sech, csch, coth, asinh, acosh, asech, acsch, acoth, deg2rad, rad2deg 
 import DualNumbers: Dual, realpart, epsilon, dual
 
 export PowerNumber, LogNumber
@@ -16,15 +19,13 @@ struct PowerNumber{T<:Number,V<:Number} <: Number
     B::T
     α::V
     β::V
-    PowerNumber{T,V}(A,B,α,β) where {T<:Number,V<:Number} = α >= β ? error("Must have α<β") : new{T,V}(A,B,α,β)
+    PowerNumber{T,V}(A,B,α,β) where {T<:Number,V<:Number} = (A,α) == (0,0) ? PowerNumber(B,0,β,Inf) :
+    α >= β ? error("Must have α<β") : new{T,V}(A,B,α,β)
 end
 
 PowerNumber(a::T, b::T, c::V, d::V) where {T,V} = PowerNumber{T,V}(a,b,c,d)
 PowerNumber(a,b,c,d) = PowerNumber(promote(a,b)..., promote(c,d)...)
-
-Base.promote_rule(::Type{PowerNumber}, ::Type{<:Number}) = PowerNumber
-Base.convert(::Type{PowerNumber}, z::PowerNumber) = z
-Base.convert(::Type{PowerNumber}, z::Number) = PowerNumber(z, 0, 0, Inf)
+PowerNumber(a,b) = PowerNumber(a,0,b,Inf)
 
 apart(z::PowerNumber) = z.A
 bpart(z::PowerNumber) = z.B
@@ -52,102 +53,86 @@ function +(x::PowerNumber, y::PowerNumber)
     return PowerNumber(tot1,tot2,γ,δ)
 end
 
-#=
-+(x::PowerNumber, y::Number) = PowerNumber(realpart(x)+y,epsilon(x), alpha(x))
-+(y::Number, x::PowerNumber) = PowerNumber(realpart(x)+y,epsilon(x), alpha(x))
++(x::PowerNumber, y::Number) = x + PowerNumber(y,0,0,Inf)
++(y::Number, x::PowerNumber) = +(x::PowerNumber, y::Number)
 
 function *(x::PowerNumber, y::PowerNumber)
-    a, b, α = realpart(x), epsilon(x), alpha(x)
-    c, d, β = realpart(y), epsilon(y), alpha(y)
-    coeff = [a*c, b*c, a*d, b*d]
-    alphs = [0, α, β, α+β]
-    N1 = PowerNumber(0,b*c,α)
-    N2 = PowerNumber(0,a*d,β)
-    N3 = PowerNumber(0,b*d,α+β)
-    if a==0 && c==0 return N3
-    elseif a==0 return N1 + N3
-    elseif c==0 return N2 + N3
-    else return a*c + N1 + N2 + N3
-    end
+    a,b,p,q = apart(x),bpart(x),alpha(x),beta(x)
+    c,d,r,s = apart(y),bpart(y),alpha(y),beta(y)
+    coeff = [a*c, b*c, a*d]
+    alphs = [p+r, q+r, p+s]
+    return PowerNumber(a*c,b*c,p+r,q+r) + PowerNumber(0,a*d,p+r,p+s)
 end
 
+*(x::PowerNumber, y::Number) = PowerNumber(y*apart(x),y*bpart(x),alpha(x),beta(x))
+*(y::Number, x::PowerNumber) = *(x::PowerNumber, y::Number)
 
-for Typ in (:Bool, :Number)
-    @eval begin
-        *(x::PowerNumber, y::$Typ) = PowerNumber(realpart(x)*y, epsilon(x)*y, alpha(x))
-        *(y::$Typ, x::PowerNumber) = PowerNumber(realpart(x)*y, epsilon(x)*y, alpha(x))
-    end
-end
-
--(x::PowerNumber) = PowerNumber(-realpart(x), -epsilon(x), alpha(x))
+-(x::PowerNumber) = PowerNumber(-apart(x),-bpart(x),alpha(x),beta(x))
 -(x::PowerNumber, y::PowerNumber) = +(x, -y)
--(x::PowerNumber, y::Number) = PowerNumber(realpart(x)-y,epsilon(x), alpha(x))
--(y::Number, x::PowerNumber) = PowerNumber(y-realpart(x),-epsilon(x), alpha(x))
+-(x::PowerNumber, y::Number) = +(x::PowerNumber, -y::Number)
+-(y::Number, x::PowerNumber) = +(-x::PowerNumber, y::Number)
 
-function inv(z::PowerNumber)
-    x, y, α = realpart(z), epsilon(z), alpha(z)
-    (α > 0.0 && x!=0) ? PowerNumber(1/x,-y/(x^2),α) : PowerNumber(0,1/y,-α) 
+function inv(x::PowerNumber)
+    a,b,p,q = apart(x),bpart(x),alpha(x),beta(x)
+    a != 0 ? PowerNumber(1/a,-b*a^(-2),-p,q-2*p) : PowerNumber(1/b,0,-q,-p)
 end
 
 /(z::PowerNumber, x::PowerNumber) = z*inv(x)
 /(z::PowerNumber, x::Number) = z*inv(x)
 /(x::Number, z::PowerNumber) = x*inv(z)
 
-==(a::PowerNumber, b::PowerNumber) = realpart(a) == realpart(b) && epsilon(a) == epsilon(b) && alpha(a) == alpha(b)
-isapprox(a::PowerNumber, b::PowerNumber; opts...) = ≈(realpart(a), realpart(b); opts...) && ≈(epsilon(a), epsilon(b); opts...) && ≈(alpha(a), alpha(b); opts...)
-
-function log(z::PowerNumber)
-    x, y, α = realpart(z), epsilon(z), alpha(z)
-    tol = 1E-14
-    if abs(x) ≤ tol || α < 0.0
-        return LogNumber(α,log(y))
-    elseif abs(x) >= 1/tol
-        return LogNumber(-α,log(y))
-    else throw(ArgumentError("Cannot eval at $z"))
-    end
+^(z::PowerNumber, p::Integer) = invoke(^, Tuple{Number,Integer}, z, p)
+function ^(z::PowerNumber, p::Number)
+    a,b,α,β = apart(z),bpart(z),alpha(z),beta(z)
+    PowerNumber(a^p,(a^(p-1))*b*p,p*α,β+(p-1)*α)
 end
 
-Complex{Float64}(a::LogNumber) = a(1)
+sqrt(z::PowerNumber) = z^0.5
+cbrt(z::PowerNumber) = z^(1/3)
 
-function atanh(z::PowerNumber)
-    x, y, α = realpart(z), epsilon(z), alpha(z)
-    if x ≈ 1
-        LogNumber(-α/2,log(2)/2  - log(-y)/2)
-    elseif x ≈ -1
-        LogNumber(α/2,-log(2)/2  + log(y)/2)
-    else
-        atanh(x)
-    end
+==(a::PowerNumber, b::PowerNumber) = apart(a) == apart(b) && bpart(a) == bpart(b) && 
+                                    alpha(a) == alpha(b) && beta(a) == beta(b)
+
+isapprox(a::PowerNumber, b::PowerNumber; opts...) = ≈(apart(a), apart(b); opts...) && ≈(bpart(a), bpart(b); opts...) && 
+                                                ≈(alpha(a), alpha(b); opts...) && ≈(beta(a), beta(b); opts...)
+
+function log(z::PowerNumber)
+    a,b,α,β = apart(z),bpart(z),alpha(z),beta(z)
+    a ≈ 0 ? (b ≈ 0 ? error("Cannot evaluate log at 0") : return LogNumber(β, log(Complex(b)))) : return LogNumber(α, log(Complex(a)))
 end
 
 log1p(z::PowerNumber) = log(z+1)
 
+function atanh(z::PowerNumber)
+    (log(1+z)-log(1-z))/2
+end
+
 for op in (:real, :imag, :conj)
-    @eval $op(l::PowerNumber) = PowerNumber($op(realpart(l)), $op(epsilon(l)), alpha(l))
+    @eval $op(l::PowerNumber) = PowerNumber($op(apart(l)), $op(bpart(l)), alpha(l), beta(l))
 end
 
-abs(l::PowerNumber) = abs(realpart(l))
+functionlist = (:abs2, :log10, :log2, :exp, :exp2, :expm1, 
+:sin, :cos, :tan, :sec, :csc, :cot, :sind, :cosd, :tand, :secd, :cscd, :cotd, :asin, :acos, 
+:atan, :asec, :acsc, :acot, :asind, :acosd, :atand, :asecd, :acscd, :acotd, :sinh, :cosh, 
+:tanh, :sech, :csch, :coth, :asinh, :acosh, :asech, :acsch, :acoth, :deg2rad, :rad2deg)
 
-function sqrt(z::PowerNumber)
-    x, y, α = realpart(z), epsilon(z), alpha(z)
-    (x!=0) ? PowerNumber(sqrt(x),y/(2*sqrt(x)),α) : PowerNumber(0,sqrt(y),α/2) 
-end
-
-# Analytic functions are trivial
-for op in (:cos, :sin, :exp) # TODO: use same list as in Dual Numbers
-	@eval function $op(z::PowerNumber)
-	    x, y, α = realpart(z), epsilon(z), alpha(z)
-	    fz = $op(dual(x,y))
-	    PowerNumber(realpart(fz), epsilon(fz), α)
+for op in functionlist
+    @eval function $op(z::PowerNumber)
+        a,b,α,β = apart(z),bpart(z),alpha(z),beta(z)
+        if α == 0
+            fz = $op(dual(a,b))
+            p = β
+        elseif α > 0
+            fz = $op(dual(0,a))
+            p = α
+        else
+            error("Alpha must be non-negative")
+        end
+	    PowerNumber(realpart(fz), epsilon(fz), 0, p)
 	end
 end
 
-^(z::PowerNumber, p::Integer) = invoke(^, Tuple{Number,Integer}, z, p)
-function ^(z::PowerNumber, p::Number)
-    x, y, α = realpart(z), epsilon(z), alpha(z)
-    (x!=0) ? PowerNumber(x^p,p*y*x^(p-1),α) : PowerNumber(0,y^p,α*p)
-end
-
+#=
 #speciallog(x::PowerNumber{<:Complex}) = alpha(x) == 1.0 ? (s = sqrt(x); 3(atanh(s)-realpart(s))/realpart(s)^3) : error("Only implemented for alpha = 1")
 
 # function SingularIntegralEquations.HypergeometricFunctions.speciallog(x::PowerNumber)
