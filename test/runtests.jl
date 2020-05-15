0d5f1834f7c6b524ba1a0c9438161521218c3e4b
idlemat <52246739+idlemat@users.noreply.github.com>
2020-05-15 22:10:00 +0200

    paper edits

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using PowerNumbers, Test, HypergeometricFunctions
+using PowerNumbers, Test, IntervalArithmetic, HypergeometricFunctions
 import PowerNumbers: PowerNumber, LogNumber, realpart, apart, bpart, alpha, beta
 
 @testset "RiemannDual -> PowerNumber" begin
@@ -11,7 +11,7 @@ import PowerNumbers: PowerNumber, LogNumber, realpart, apart, bpart, alpha, beta
         @test log1p(PowerNumber(-1,a,0,1))(h) ≈ log(h*a)
         @test log1p(PowerNumber(a,Inf,-1,0))(h) ≈ log(a/h)
     end
- 
+
     h=0.0001
     for z in (PowerNumber(1,3exp(0.2im),0,1), PowerNumber(1,0.5exp(-1.3im),0,1),
               PowerNumber(-1,3exp(0.2im),0,1), PowerNumber(-1,0.5exp(-1.3im),0,1),
@@ -41,14 +41,14 @@ end
     @test real(PowerNumber(2im,im+1,0,0.5)) == PowerNumber(0,1,0,0.5)
     @test imag(PowerNumber(2im,im+1,0,0.5)) == PowerNumber(2,1,0,0.5)
     @test conj(PowerNumber(2im,im+1,0,0.5)) == PowerNumber(-2im,1-im,0,0.5)
-    
+
     @test (3*PowerNumber(0.01+3im,0.2im+1,0,0.4) - 4*PowerNumber(0.6-1.5im,1.5-0.3im,0,0.4))/3 == PowerNumber(-0.79+5.0im,-1.0 + 0.6im,0,0.4)
 end
 
 @testset "sin" begin
     ε = PowerNumber(1,1)
-    @test sin(sqrt(ε))^2 === PowerNumber(1.0,1.0)	
-    @test sin(sqrt(ε))^2.0 === PowerNumber(1.0,1.0)	
+    @test sin(sqrt(ε))^2 === PowerNumber(1.0,1.0)
+    @test sin(sqrt(ε))^2.0 === PowerNumber(1.0,1.0)
 end
 
 @testset "LogNumber" begin
@@ -74,6 +74,12 @@ end
     a,b,c = 1.1,1.2,1.3
     @test_broken _₂F₁(a,b,c,z)
     log1p(-z)
+end
+
+@testset "IntervalArithmetic" begin
+    a = @interval(1.0)
+    p = PowerNumber(a,a,0,1)
+    @test (a^4+a^2-a) == (p^4+p^2-p)(0)
 end
 
 z/(1-z) |> typeof
